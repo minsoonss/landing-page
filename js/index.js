576556e11461497a0d6a3c7c50f3bedbc1461540
HS_KKJ <hotsellerofficial@gmail.com>
2021-03-28 21:42:00 +0900

    UPDATE 210328.04

--- js/index.js
+++ js/index.js
@@ -1,27 +1,71 @@
 const $scroll = $('html, body');
-var num = 0;
 
 function menuSlide(){
   $('.menuBtn').toggleClass('on');
   $('.menu').toggle();
   $('.bgShadow').toggle();
+  if($(window).scrollTop() <= 0){
+    $('.kakao').toggleClass('fixedOn')
+    $('.kakao').css({transition: 'none'})
+  }
 };
 
 $(function(){
+  var windowWidth = $(window).width();
+  var scrollMT = 80;
+
+  /*모바일 초기값*/
+  function mobileInit(){
+    $('.visual .txtBox').removeClass('wow fadeIn');
+    $('.smartphone').removeClass('wow slideInRight');
+    if(windowWidth >= 768){
+      scrollMT = 80;
+      $('.kakao').text('카카오톡 상담');
+    }else{
+      scrollMT = 60;
+      $('.kakao').text('바로 상담하기');
+    }
+    if(windowWidth >= 768 && $(window).scrollTop() > 0){
+      $('.kakao').addClass('fixedOn');
+      $('.kakao').css({transition: 'none'})
+    }
+  }
+
+  mobileInit();
+
+  /*RESIZE*/
+  $(window).resize(function(){
+    windowWidth = $(this).width();
+    if(windowWidth >= 768){
+      scrollMT = 80;
+      $('.kakao').text('카카오톡 상담');
+    }else{
+      scrollMT = 60;
+      $('.kakao').text('바로 상담하기');
+    }
+  })
+
   /*MENU*/
   $('.menu li:not(:last-child) a').click(function(){
     const menuMove = $(this).attr('href');
     const scrollValue = $(menuMove).offset().top;
-    $scroll.stop().animate({'scrollTop': `${scrollValue - 80}px`}, 1000)
+    $scroll.stop().animate({'scrollTop': `${scrollValue - scrollMT}px`}, 1000)
   });
   
   /*GRAPH*/
   $(window).scroll(function(){
     const scrollNum = $(this).scrollTop();
     const $circle = $('.insta .imgBox .graph svg circle');
+    console.log(scrollNum)
     if(scrollNum >= 500){
       $circle.animate({'stroke-dashoffset': '32.5px'}, 1200);
     };
+    if(scrollNum > 0){
+      $('.kakao').addClass('fixedOn');
+      $('.kakao').css({transition: 'all 0.4s ease-in-out'})
+    }else{
+      $('.kakao').removeClass('fixedOn')
+    }
   });
   
   /*SWIPER*/
